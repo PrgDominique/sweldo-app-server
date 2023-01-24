321bfc8fa98e37cc4a2d9bdaa504c141dd410fd6
jasonlerit <jasonlerit077@gmail.com>
2023-01-24 12:05:51 +0800

    added forgot password mail

---
+++ resources/views/emails/forgot-password.blade.php
@@ -0,0 +1,6 @@
+<div class="container" style="padding: 1rem; background: #f5f5f5;">
+    <p>Forgot Password</p>
+    <p>
+        Please click the link to reset your password {{ $link }}
+    </p>
+</div>
