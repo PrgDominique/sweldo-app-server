75ccecffae92b7ea964d14ac4eb2364285a85351
PrgDominique <dominiquesimbillo10@gmail.com>
2023-01-25 22:16:39 +0800

    added seed and announcement controller

--- app/Http/Controllers/User/AnnouncementController.php
+++ app/Http/Controllers/User/AnnouncementController.php
@@ -6,5 +6,21 @@ use Illuminate\Http\Request;
 
 class AnnouncementController extends Controller
 {
-    //
+    //fetch all announcements from the database
+    public function index(Request $request)
+    {
+        $announcements = $request->user()->announcements()->orderBy('created_at', 'desc')->get();
+
+        return response()->json([
+            'announcements' => $announcements
+        ]);
+    }
+
+    
+    //add function that will generate fake announcements
+
+ 
+
+
+
 }
